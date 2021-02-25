304927ac4316f23a1f127eb7c07fc2c965de6019
Meaxy <53201730+Meaxy76@users.noreply.github.com>
2021-02-25 11:23:31 -0500

    Add files via upload

---
+++ eva_kernel.c
@@ -0,0 +1,16 @@
+
+/* EvaMSX Kernel prototype x86_64                   */
+/* Used for Aura Prototype 0A                       */
+/* V0.0.1                                           */
+
+void eva_kernel_main(void)
+{
+     printf("Japanese not supported yet... \n");
+     printf("Welcome to EvaMSX Kernel!\n");
+     printf("Willkommen im EvaMSX-Kernel!\n");
+     printf("Bienvenue sur le kernel EvaMSX!\n");
+}
+
+
+
+
